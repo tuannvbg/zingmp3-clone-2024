758cedc1eb1bb622a36b4d0ce9ce092b06833446
Linhyum <phiep860974@gmail.com>
2023-09-10 17:16:15 +0700

    fix bug

--- src/hooks/useAddLibrary.ts
+++ src/hooks/useAddLibrary.ts
@@ -4,10 +4,13 @@ import { HomeListType } from '@/types/homelist.type'
 import { AppContext } from '@/contexts/app.context'
 import { SongItem } from '@/types/playlist.type'
 import { Data } from '@/types/video.type'
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-toastify'
 
 export default function useAddLibrary() {
    const [library, setLibrary] = useLocalStorage<string[]>('library', []) //thêm vào thư viện
-   const { setPlaylistLibrary, setSongsLibrary, setVideoLibrary } = useContext(AppContext)
+   const { setPlaylistLibrary, setSongsLibrary, setVideoLibrary, isAuthenticated } = useContext(AppContext)
+   const router = useRouter()
    //thêm vào thư viện
    const handleAddLibrary = (
       e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -17,28 +20,33 @@ export default function useAddLibrary() {
       video?: Data
    ) => {
       e.stopPropagation()
-      if (library.includes(id)) {
-         setLibrary((prev) => prev.filter((item) => item !== id))
-         if (playlist) {
-            setPlaylistLibrary((prev) => (prev as HomeListType[]).filter((item) => item.encodeId !== id))
-         }
-         if (song) {
-            setSongsLibrary((prev) => prev.filter((item) => item.encodeId !== id))
-         }
-         if (video) {
-            setVideoLibrary((prev) => prev.filter((item) => item.encodeId !== id))
+      if (isAuthenticated) {
+         if (library.includes(id)) {
+            setLibrary((prev) => prev.filter((item) => item !== id))
+            if (playlist) {
+               setPlaylistLibrary((prev) => (prev as HomeListType[]).filter((item) => item.encodeId !== id))
+            }
+            if (song) {
+               setSongsLibrary((prev) => prev.filter((item) => item.encodeId !== id))
+            }
+            if (video) {
+               setVideoLibrary((prev) => prev.filter((item) => item.encodeId !== id))
+            }
+         } else {
+            setLibrary((prev) => [...prev, id])
+            if (playlist) {
+               setPlaylistLibrary((prev) => [...prev, playlist])
+            }
+            if (song) {
+               setSongsLibrary((prev) => [...prev, song])
+            }
+            if (video) {
+               setVideoLibrary((prev) => [...prev, video])
+            }
          }
       } else {
-         setLibrary((prev) => [...prev, id])
-         if (playlist) {
-            setPlaylistLibrary((prev) => [...prev, playlist])
-         }
-         if (song) {
-            setSongsLibrary((prev) => [...prev, song])
-         }
-         if (video) {
-            setVideoLibrary((prev) => [...prev, video])
-         }
+         toast.warning('Vui lòng đăng nhập')
+         router.push('/login')
       }
    }
    return { library, handleAddLibrary }
